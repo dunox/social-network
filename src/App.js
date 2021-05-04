26ebbd6a599b8322698b0fd268b9e07a2a061b62
Andriy Shendryk <avshendrik@gmail.com>
2021-05-04 22:07:40 +0300

    add dialogs component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { SideBar } from './components/sidebar';
 import { Header } from './components/header';
 import { Profile } from './components/profile';
-
+import { Dialogs } from './components/dialogs';
 import "./App.css";
+
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <SideBar />
-      <Profile />
+      <div className="App-content">
+        {/* <Profile /> */}
+      <Dialogs />
+      </div>
     </div>
   );
 }
